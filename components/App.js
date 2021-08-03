81dd16a766880c9ac374151e825eb545fc467985
kamurtiveena <kamurtiveena@gmail.com>
2021-08-03 20:25:46 +0530

    2 files modified, 3 added, 3 deleted

--- components/App.js
+++ components/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Footer from './Footer'
-import AddTodo from '../containers/AddPlayer'
-import VisibleTodoList from '../containers/VisibleTodoList'
+import AddPlayer from '../containers/AddPlayer'
+import VisiblePlayerList from '../containers/VisiblePlayerList'
 
 const App = () => (
   <div>
     <div style={{color:"#4d1073",align:"center", padding:"20%"}}> Welcome To Vico Binary Team </div>
-    <AddTodo />
-    <VisibleTodoList />
+    <AddPlayer />
+    <VisiblePlayerList />
     <Footer />
   </div>
 ) 
--- containers/VisiblePlayerList.js
+++ containers/VisiblePlayerList.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux'
 import { toggleTodo } from '../actions'
-import TodoList from '../components/TodoList'
+import TodoList from '../components/PlayerList'
 
 const getVisibleTodos = (todos, filter) => {
   switch (filter) {
